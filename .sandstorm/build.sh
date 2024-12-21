45436b2367c6069eba68d066e4afaa43cfe87f82
Daniel Krol <orblivion@gmail.com>
2024-12-21 12:29:51 -0500

    WIP

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -5,8 +5,11 @@ set -euo pipefail
 here="$(dirname "$(readlink -f "$0")")"
 cd $here/..
 
-#make web
-make cli-linux-amd64
+# TODO - is there a better place for this?
+export PATH=$PATH:~/go/bin
 
 goreleaser -v || go install github.com/goreleaser/goreleaser@latest
 goreleaser -v   # verifies that it worked
+
+#make web
+make cli-linux-amd64
