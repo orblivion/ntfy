e2175f517015e34deff1f6aca68eca7f7ea163b2
Daniel Krol <orblivion@gmail.com>
2024-12-22 00:30:05 -0500

    WIP

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -8,7 +8,7 @@ set -euo pipefail
 export NTFY_CACHE_FILE=/var/lib/ntfy/cache.db
 export NTFY_CACHE_DURATION=48h # Default 12h
 #export NTFY_AUTH_DEFAULT_ACCESS=... (TODO: I think default of read-write all is fine)
-export NTFY_BEHIND_PROXY=true # (TODO: Hopefully Sandstorm uses X-Forwarded-For header)
+export NTFY_BEHIND_PROXY=true # (TODO: Hopefully Sandstorm uses X-Forwarded-For header. Check this actually, it uses it for rate limiting.)
 #export NTFY_ATTACHMENT_CACHE_DIR=/var/lib/ntfy/attachments (TODO: Seems to require BASE_URL for some reason)
 # export NTFY_ENABLE_LOGIN=true (TODO: default seems to have me just logged in anyway?)
 
@@ -20,6 +20,7 @@ mkdir -p /var/lib/ntfy/attachments
 export PATH=$PATH:/opt/app/dist/ntfy_linux_amd64_linux_amd64_v1
 
 # TODO - Caddy config. Put web root in a separate box that is not accessible via the API endpoint
+# https://docs.ntfy.sh/config/#nginxapache2caddy for an idea
 
 cd /opt/app
 ntfy serve
