b6c1062361f6b36fe924f494f8602e8effc7d8dd
Daniel Krol <orblivion@gmail.com>
2024-12-23 19:47:04 -0500

    WIP export PATH for go in build.sh

--- .sandstorm/build.sh
+++ .sandstorm/build.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 here="$(dirname "$(readlink -f "$0")")"
 cd $here/..
 
+# TODO - this is really annoying, I shouldn't have to do this everywhere
+export PATH=$PATH:/usr/local/go/bin
 # TODO - is there a better place for this?
 export PATH=$PATH:~/go/bin
 
