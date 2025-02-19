7c80985fa6a02f7f785905ca1b402ecd99effbd3
Daniel Krol <orblivion@gmail.com>
2025-02-18 19:37:34 -0500

    WIP First version of "nice" Sandstorm ntfy onboarding interface

--- web/src/components/Sandstorm.jsx
+++ web/src/components/Sandstorm.jsx
@@ -5,13 +5,11 @@ import {
   Alert,
   AlertTitle,
   Stack,
-  Button,
   Container,
-  ListItemText,
+  Link,
 } from "@mui/material";
-import { WavingHand } from "@mui/icons-material";
+import { WavingHand, AppSettingsAlt, MobileFriendly, InstallMobile } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
-import { InstallMobile } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
 import routes from "./routes";
 import { requestSandstormIframeURL } from "../app/sandstorm";
@@ -21,41 +19,81 @@ export const Welcome = () => (
   <Container maxWidth="md" sx={{ marginTop: 3, marginBottom: 3 }}>
     <Stack spacing={3}>
       <Intro/>
-      <AppSetup/> {/* Or else link to settings from here. Maybe worth it for the space savings? */}
+      <ConnectingApps/>
+      <Scripts/>
       <MissingFeatures/>
-      <SecurityAndPrivacy/>
     </Stack>
   </Container>
 );
 
 const Intro = () => {
-  const navigate = useNavigate();
   const { t } = useTranslation(); // TODO
   return (
     <Card sx={{ p: 3 }} aria-label="Welcome to ntfy for Sandstorm">
-      <Typography variant="h5" sx={{ marginBottom: 2 }}>
-        Welcome to ntfy for Sandstorm
-      </Typography>
-      This is for <a href="https://unifiedpush.org/">UnifiedPush</a>. {/*TODO explain more*/}
-      See below for specal security and privacy issues.
-      If you're familiar with ntfy, see below for some differences you should know about.
+      <CardContent>
+        <Typography variant="h5" sx={{ marginBottom: 2 }}>
+          Welcome to ntfy for Sandstorm
+        </Typography>
+        <p>
+          <b>ntfy</b> is a notification service. It integrates with many open source Android applications, and lets you create your own custom scripts and applications.
+        </p>
+        <p>
+          Read below to learn about:
+	  <ul>
+	    <li>How to use this app</li>
+	    <li>How it differs from usual ntfy servers <i>and</i> usual Sandstorm apps</li>
+	    <li>Some security and privacy considerations</li>
+	  </ul>
+        </p>
+      </CardContent>
     </Card>
   )
 };
 
-const SecurityAndPrivacy = () => {
+const ConnectingApps = () => {
   const navigate = useNavigate();
   return (
-    <Card sx={{ p: 3 }} aria-label="Security And Privacy">
+    <Card sx={{ p: 3 }} aria-label="Connecting Apps">
       <CardContent>
         <Typography variant="h5" sx={{ marginBottom: 2 }}>
-          Security And Privacy
+          Connecting Apps
         </Typography>
-        Unlike most Sandstorm applications, ntfy takes connections from any services you connect to.
-        <br/><br/>
+        <p>
+          <b>ntfy</b> implements <Link href="https://unifiedpush.org/">UnifiedPush</Link>, which gives you push notifications for your open source Android apps without requiring Google services.
+        </p>
+        <CardContent> {/* I wanted another indent */}
+          <p><InstallMobile/> Install the ntfy Android app (available on <Link href="https://f-droid.org/en/packages/io.heckel.ntfy/">F-Droid</Link> and <Link href="https://play.google.com/store/apps/details?id=io.heckel.ntfy">Play Store</Link>).</p>
+          <p><Link href="#" onClick={() => navigate(routes.settings)}><AppSettingsAlt/> Connect your ntfy Android app to this grain</Link>.</p>
+          <p><MobileFriendly/> Set your ntfy-enabled apps to use ntfy for notifications.</p>
+        </CardContent>
         <Alert severity="warning" sx={{ paddingTop: 2 }}>
-          <AlertTitle>Privacy Warning</AlertTitle>
-          Some apps, such as Tusky, will <b>automatically configure itself</b>. Beware! But you can always revoke.
+          <AlertTitle>Privacy and Security</AlertTitle>
+          Unlike most Sandstorm applications, ntfy-enabled apps will give your API URL to 3rd party services (Mastodon, Matrix, etc) in order to recieve push notifications. Some apps, such as Tusky, will <b>automatically configure themseles</b> to use ntfy and pass on your API URL <b>without asking you first</b>.
+          <br/>
+          <br/>
+          If you are concerned that your API URL has been compromised, you can revoke it from the Sandstorm Webkeys menu (next to grain sharing, etc).
+        </Alert>
+      </CardContent>
+    </Card>
+  )
+}
+
+const Scripts = () => {
+  return (
+    <Card sx={{ p: 3 }} aria-label="Custom Scripts and Applications">
+      <CardContent>
+        <Typography variant="h5" sx={{ marginBottom: 2 }}>
+          Custom Scripts and Applications
+        </Typography>
+	<p>
+          You can use ntfy in your own <Link href="https://docs.ntfy.sh/publish/">scripts and applications</Link> by simply sending a POST request. However, you must use the <Link href="https://docs.ntfy.sh/publish/">JSON request</Link> option. (Sandstorm will not allow the custom headers that ntfy uses).
+	</p>
+	<p>
+	  See some <Link href="https://docs.ntfy.sh/examples/">examples</Link>.
+	</p>
+        <Alert severity="warning" sx={{ paddingTop: 2 }}>
+          <AlertTitle>Privacy and Security</AlertTitle>
+          Treat topics like <b>passwords</b>. Any connected 3rd party services can read any topic from your grain if they know its name, so make it hard to guess!
         </Alert>
       </CardContent>
     </Card>
@@ -63,30 +101,29 @@ const SecurityAndPrivacy = () => {
 }
 
 const MissingFeatures = () => {
-  const navigate = useNavigate();
   return (
     <Card sx={{ p: 3 }} aria-label="Missing Features">
       <CardContent>
         <Typography variant="h5" sx={{ marginBottom: 2 }}>
           Missing Features
         </Typography>
-        If you're familiar with ntfy, there are a handful of features that you might be missing.
-        <ul>{/*<List> ?*/}
-          <li>Protected Topics (Authentication)</li>
+        If you're familiar with <b>ntfy</b>, there are a handful of features that you might be missing in the Sandstorm version:
+        <ul>
+          <li>Protected Topics</li>
           <li>Desktop Notifications</li>
           <li>Progressive Web App (PWA)</li>
           <li>Web subscriptions (other than for testing purposes)</li>
-          <li>Sending messages with headers</li>
+          <li>Sending messages with headers (JSON only)</li>
+          <li>Some 3rd party services</li>
         </ul>
-        <a href="https://github.com/orblivion/ntfy/blob/sandstorm/.sandstorm/README_MISSING_FEATURES.md">
+        <Link href="https://github.com/orblivion/ntfy/blob/sandstorm/.sandstorm/README_MISSING_FEATURES.md">
           Learn More
-        </a>
+        </Link>
       </CardContent>
     </Card>
   )
 }
 
-// TODO - Something about UnifiedPush here? How to configure apps, etc?
 export const AppSetup = () => {
   const { t } = useTranslation(); // TODO
   useEffect(() => {
@@ -98,17 +135,20 @@ export const AppSetup = () => {
         <Typography variant="h5" sx={{ marginBottom: 2 }}>
           App Setup
         </Typography>
-        <InstallMobile/>
-        <b>On Android and iOS:</b> <span style={{ display: "inline-block" }}><i>Settings&rarr;General&rarr;Default Server</i></span>
-        <br />
-        <iframe scrolling="no" style={{ "background-color": "#ffffff", height: "15px", width: "100%", "margin-left": 0, "margin-top": "15px", "margin-bottom": "15px", border: 0, overflow: "hidden" }} id="offer-iframe-full"></iframe>
-        <br />
-        This URL will also work for custom scripts. <a href="https://docs.ntfy.sh/publish/">See here, though note that only the JSON API works.</a>.
-        <br />
-        <br />
+	<p>
+          Here you will set up your <b>API URL</b>. This is what you will use to connect to your ntfy app on your phone, as well as your custom scripts and applications.
+	</p>
+	<p>
+          <AppSettingsAlt/>
+          <b>On Android and iOS:</b> <span style={{ display: "inline-block" }}><i>Settings&rarr;General&rarr;Default Server</i></span>
+	</p>
+	<p>
+          <iframe scrolling="no" style={{ "background-color": "#ffffff", height: "15px", width: "100%", "margin-left": 0, "margin-top": "15px", "margin-bottom": "15px", border: 0, overflow: "hidden" }} id="offer-iframe-full"></iframe>
+	</p>
         <Alert severity="info" sx={{ paddingTop: 2 }}>
-          <AlertTitle>Revoking</AlertTitle>
-          You can always revoke API URLs from the Sandstorm Webkeys menu (next to grain sharing, etc) and get a new one here.
+          <AlertTitle>In Case of Compromise</AlertTitle>
+	  {/* TODO explain that you might end up with multiple API keys. And that this is probably not great for this app? You probably just want the "default server". Actually, maybe do this in the SecurityAndPrivacy thing. And maybe just make reference to that here to "make sure you see" it. */}
+          If you are concerned that your API URL has been compromised, you can revoke it from the Sandstorm Webkeys menu (next to grain sharing, etc) and get a new one here.
         </Alert>
       </CardContent>
     </Card>
