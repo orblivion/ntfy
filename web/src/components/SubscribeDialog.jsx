9cb5f65681f4201b0e3cf2e042e327d7429ae8e7
Daniel Krol <orblivion@gmail.com>
2025-02-27 15:17:32 -0500

    WIP disable UI for unavailable features

--- web/src/components/SubscribeDialog.jsx
+++ web/src/components/SubscribeDialog.jsx
@@ -217,7 +217,7 @@ const SubscribePage = (props) => {
             {reserveTopicVisible && <ReserveTopicSelect value={everyone} onChange={setEveryone} />}
           </FormGroup>
         )}
-        {!reserveTopicVisible && (
+        {!reserveTopicVisible && false ( // Sandstorm ntfy doesn't interact with other ntfy servers right now
           <FormGroup>
             <FormControlLabel
               control={
