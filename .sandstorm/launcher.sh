6ca82ee3c7a801687948471e179cd9de9d050ce5
Daniel Krol <orblivion@gmail.com>
2025-03-03 22:31:23 -0500

    WIP don't need an attachments dir oops

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -10,9 +10,6 @@ export NTFY_CACHE_FILE=/var/lib/ntfy/cache.db
 # See changes in web/src/components/routes.js
 export NTFY_DISALLOWED_TOPICS="sandstorm-extra-89dfdbfd72e2ae64728dd"
 
-# Gets everything underneath as well
-mkdir -p /var/lib/ntfy/attachments
-
 export PATH=$PATH:/opt/app/dist/ntfy_linux_amd64_linux_amd64_v1
 
 cd /opt/app
