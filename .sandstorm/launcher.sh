e187638017fa7fd28e3e9dd0f0cac30474b125c1
Daniel Krol <orblivion@gmail.com>
2025-03-18 21:57:00 -0400

    WIP increase message burst max rate

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -10,6 +10,11 @@ export NTFY_LOG_LEVEL="trace" # TODO - remove this before launch! "info" is the 
 # See changes in web/src/components/routes.js
 export NTFY_DISALLOWED_TOPICS="sandstorm-extra-89dfdbfd72e2ae64728dd"
 
+# Default is 60. Increasing because we can't have per-visitor rate limiting.
+# We could be subscribed to as many as 30 topics but I seriously doubt all
+# 30 will be bursting at the same time. Let's say maybe as many as four are.
+export NTFY_VISITOR_REQUEST_LIMIT_BURST=360
+
 export PATH=$PATH:/opt/app/dist/ntfy_linux_amd64_linux_amd64_v1
 
 cd /opt/app
