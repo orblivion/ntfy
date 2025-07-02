b4ec71adc9c1ad0d681cdfa7649a831354102b8b
Daniel Krol <orblivion@gmail.com>
2025-07-02 12:59:28 -0400

    WIP Don't use forSharing in offer template

--- server/sandstorm.go
+++ server/sandstorm.go
@@ -5,6 +5,17 @@ import (
 	"strings"
 )
 
+// NOTE - Do not trust Sandstorm User ID for security!
+//
+// Requests from third parties via an API URL will have the grain owner's
+// User ID (at least, I think it does? per the Sandstorm docs). That's why we
+// rely on permissions, which are attached to the API URL. (If we used forSharing
+// I think it would strip the User ID, but then it would put the API tokens in
+// the sharing menu instead of the API tokens menu.)
+//
+// The only reason I can see for looking at Sandstorm User ID is for grain
+// sharing, which I see no good reason for as of this writing.
+
 type SandstormPermission string
 
 const HeaderSandstormPermissions = "X-Sandstorm-Permissions"
