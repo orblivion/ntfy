175b818319b87b211e409f27cc86dcedc990d746
Daniel Krol <orblivion@gmail.com>
2025-07-02 17:05:46 -0400

    WIP install the latest golang

--- .sandstorm/installers/install-go.sh
+++ .sandstorm/installers/install-go.sh
@@ -4,10 +4,10 @@ here="$(dirname "$(readlink -f "$0")")"
 cd "$here"
 
 # Don't waste time installing it twice
-go version | grep 'go version go1.21.6 linux/amd64' && exit 0
+go version | grep 'go version go1.24.4 linux/amd64' && exit 0
 
 # https://golang.org/doc/install
-wget https://golang.org/dl/go1.21.6.linux-amd64.tar.gz -O /tmp/golang.3e9a2640b23f279fabc6c5ed40d101f1.tar.gz
+wget https://golang.org/dl/go1.24.4.linux-amd64.tar.gz -O /tmp/golang.3e9a2640b23f279fabc6c5ed40d101f1.tar.gz
 sha256sum --strict -c golang.checksum
 
 # https://golang.org/doc/install
