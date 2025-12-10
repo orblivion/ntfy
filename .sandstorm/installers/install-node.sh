32d86790438e110bbd31026c83a379cc523e2edb
Daniel Krol <orblivion@gmail.com>
2025-12-09 23:27:11 -0500

    Upgrade node

--- .sandstorm/installers/install-node.sh
+++ .sandstorm/installers/install-node.sh
@@ -3,6 +3,6 @@ set -euox pipefail
 
 # Not great perhaps that I'm running curl and npm as root but this is just for
 # Sandstorm build and dev environments.
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
 apt-get install -y nodejs
 npm -v   # verifies that it worked
