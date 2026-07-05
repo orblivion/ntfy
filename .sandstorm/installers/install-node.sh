d7f725b544e9bf7a8f9b9ae3b96bb9a4579ab518
Daniel Krol <orblivion@gmail.com>
2026-07-05 18:22:22 -0400

    Sandstorm: Try Trixie's node and npm
    Had some weird problems with the nodesource one. But the Trixie one, I
    think, successfully built the front end. So maybe we'll keep it simple
    and do this.

--- .sandstorm/installers/install-node.sh
+++ .sandstorm/installers/install-node.sh
@@ -3,7 +3,6 @@ set -euox pipefail
 
 # Not great perhaps that I'm running curl and npm as root but this is just for
 # Sandstorm build and dev environments.
-curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
-apt install -y nodejs
-apt install -y npm
+#curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
+apt install -y nodejs npm
 npm -v   # verifies that it worked
